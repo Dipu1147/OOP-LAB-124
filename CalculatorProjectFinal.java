8c997e6ee5526984c281994ad64de0c1b4ff112b
Dipu1147 <118482004+Dipu1147@users.noreply.github.com>
2023-11-08 12:53:33 +0600

    CalculatorProjectFinal.java

--- CalculatorProjectFinal.java
+++ CalculatorProjectFinal.java
@@ -95,9 +95,12 @@ public class CalculatorProjectFinal {
         btnpm = new JButton("+/-");
         btnpm.setBackground(new Color(153, 153, 204));
         btnpm.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            }
-        });
+    public void actionPerformed(ActionEvent e) {
+        double currentValue = Double.parseDouble(lblans.getText());
+        currentValue *= -1;
+        lblans.setText(String.valueOf(currentValue));
+    }
+});
         btnpm.setBounds(67, 83, 49, 23);
         frame.getContentPane().add(btnpm);
 
